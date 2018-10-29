90a87e1c498ebe9aec036df6837bede840cdd08b
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-10-29 14:22:34 +0900

    少し追加した

--- main.cc
+++ main.cc
@@ -20,13 +20,15 @@
 #include <tuple>
 #include <iomanip>
 
-#define rrep(i,n) for(int i=n;i>0;i--)
-#define RREP(i,a,n) for(int i=n;i>a;i--)
+#define rrep(i,n) for(int i=n-1;i>=0;i--)
+#define RREP(i,a,n) for(int i=n-1;i>=a;i--)
 #define rep(i,n) for(int i=0;i<n;i++)
 #define REP(i,a,n) for(int i=a;i<n;i++)
 #define all(a) (a).begin(),(a).end()
 #define pb(x) push_back(x)
 #define mp(x,y) make_pair((x),(y))
+#define sz(x) ((int)(x).size())
+#define uniq(v) v.erase( unique(v.begin(), v.end()), v.end() );
 
 #define fs first
 #define sc second
@@ -55,6 +57,9 @@ using Mii = M<int, int>;
 using Mll = M<ll, ll>;
 using Msi = M<string, int>;
 
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
 template<class T> istream& operator >>(istream &is, vector<T> &v){for(T &e:v)is>>e;return is;}
 template<class T> ostream& operator <<(ostream &os, vector<T> v){os<<"{";for(T &e:v)os<<e<<(v.size()-(int)(&e-&v[0])>1?", ":"");os<<"}";return os;}
 
@@ -74,6 +79,18 @@ template<class H,class... T> void resz(int n,H& a,T&... b){a.resize(n);resz(n,b.
 #define DY {0,-1,0,1,-1,1,1,-1}
 #define DX2 {-1,0,1,0}
 #define DY2 {0,-1,0,1}
+
+// 最大公約数
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+// 最大公倍数
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+
+// io 高速化
+struct io{
+    io(){
+        cin.tie(0); ios::sync_with_stdio(0); cout<<fixed<<setprecision(20);
+    };
+}io;
 
 signed main() {
 
